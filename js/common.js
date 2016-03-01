9d0516a40c4baa6f7760bf51c654283afe72f116
kirslb <kir-sl@ya.ru>
2016-03-01 22:51:57 +0500

    4h6m

--- js/common.js
+++ js/common.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
 	//equalheight - одинаковая высота колонок
 	//Пример списка элементов:
 	//var eqElement = ".cat_container > div, .home_news > div"
-	var eqElement = ".element"
+	var eqElement = ".hi_item"
 	$(window).load(function(){equalheight(eqElement);}).resize(function(){equalheight(eqElement);});
 
 	//Masked Input Plugin
